d28d85a3c7c0d62b6b002da4ea605c5f7ef06558
NoahS1010 <noahsmolarz@gmail.com>
2026-01-19 15:04:07 -0500

    Add files via upload

--- Lab1.py
+++ Lab1.py
@@ -3,20 +3,27 @@ def myhelloworld():
     
 myhelloworld()
 
+def input_output():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    height = float(input("Enter your height: "))
+    
+    print(f"Hello, {name}!")
+    print(f"You are {age} years old.")
+    print(f"Your height is {height} meters.")
+
+input_output()
+
+
+
+#Extra code. Different way to do it.
 def questionnaire():
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     height = float(input("Enter your height: "))
+    
     print("Hello, ",name,"!")
     print("You are ",age," years old.")
     print("Your height is ",height,"meters.")
-#questionnaire()
 
-def questionnaire2():
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
-    height = float(input("Enter your height: "))
-    print(f"Hello, {name}!")
-    print(f"You are {age} years old.")
-    print(f"Your height is {height} meters.")
-questionnaire2()
+#questionnaire()
